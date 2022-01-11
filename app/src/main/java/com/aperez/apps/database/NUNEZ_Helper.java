500dd14ce4a236d78434f59632ba50ca18f49984
santisebas97 <nunezsantiago1e@gmail.com>
2022-01-10 19:49:33 -0500

    commit 19:49

--- app/src/main/java/com/aperez/apps/database/NUNEZ_Helper.java
+++ app/src/main/java/com/aperez/apps/database/NUNEZ_Helper.java
@@ -1,4 +1,28 @@
 package com.aperez.apps.database;
 
-public class NUNEZ_Helper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class NUNEZ_Helper extends SQLiteOpenHelper {
+
+    private String createTable_Usuarios="CREATE TABLE Usuarios(Codigo INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "Nombre TEXT," +
+            "Contrasena TEXT)";
+
+    public NUNEZ_Helper(@Nullable Context context, @Nullable String name,
+                        @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
